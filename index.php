28acc6550061a119f8572f03b47d71840faf20fc
091910584 Nancy <nancy.estradaa17@gmail.com>
2022-03-13 14:14:18 -0600

    Primer Screenshot del Proyecto GitMundo "Nancy"

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Pagina Principal</title>
+    </head>
+    <body>
+        <h1>Saludos Cordiales</h1>
+    </body>
+    
+</html> 
