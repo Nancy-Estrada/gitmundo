3f0abd58e85fccd1b6d17fb371277f6ad96482f6
091910584 Nancy <nancy.estradaa17@gmail.com>
2022-03-13 19:06:37 -0600

    SNAPSHOT 002 del Proyecto, sólo modifucamos index

---
+++ contacto.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Pagina Principal</title>
+    </head>
+    <body>
+        <h1>Contacto</h1>
+        <h2> Número Teléfonico</h2>
+        <h3>9681139867</h3>
+        <h2>Correo</h2>
+        <h3>nancy.estradaa17@gmail.com</h3>
+    </body>
+    
+</html> 
--- index.php
+++ index.php
@@ -5,7 +5,22 @@
         <title>Pagina Principal</title>
     </head>
     <body>
-        <h1>Saludos Cordiales</h1>
+        <font size = '14' name="Tempus Sans TIC" color="Blue">
+            <Marquee>
+            Saludos Cordiales TICsitos
+            </Marquee>
+        </font>
+        <h2>
+            DESARROLLO WEB
+        </h2>
+        <h3 style="text-align: justify">
+             El desarrollo web es, la construcción y el mantenimiento de los sitios web,
+             lo que incluye a las aplicaciones. Lo que se ve y lo que no se ve. 
+             Que un sitio web sea atractivo, tenga una apariencia fantástica, funcione tan rápidamente 
+             como el visitante lo demanda y permita una excelente experiencia de usuario depende del 
+             desarrollador. Así como, es su tarea que se exhiban en forma práctica los productos o 
+             mensajes y, que pueda ser un sitio web auto administrable.
+        </h3>
     </body>
     
 </html> 
